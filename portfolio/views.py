014ee90e31677600ded107eafe28792daedf9d2e
Crisz Mendoza <criszmendoza212@gmail.com>
2022-03-08 13:52:51 -0600

    Vista de proyectos

--- portfolio/views.py
+++ portfolio/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
+from .models import Proyect as Project
 
 # Create your views here.
 
 
 def home(request):
-    return render(request, 'home.html')
+    projects = Project.objects.all()
+    return render(request, 'home.html', {'projects': projects})
